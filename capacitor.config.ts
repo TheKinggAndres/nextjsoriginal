c1d2eb12396efce6797ad17ca6ed5cccc4ec4bd1
TheKinggAndres <puentes.andres.9090@gmail.com>
2025-11-26 02:02:56 +0000

    archivo android listo con capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nextjsoriginal.app',
+  appName: 'nextjsoriginal',
+  webDir: 'out'
+};
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,  // Necesario si usas el componente Image de Next.js
+  },
 };
 
 export default nextConfig;
